7dd2cf9c66a24bd6ee6e0deb9328fd868ddd58a3
Scott Fleming <sdflem@gmail.com>
2017-02-01 19:54:52 +0000

    Finished Lisa task.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
+  get 'static_pages/lisa'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
